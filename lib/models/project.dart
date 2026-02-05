d7221220a0556b86481e45d126d6e82b1418871c
Егор <tujhustify@gmail.com>
2026-02-05 22:59:32 +0300

    Notifications, Project
    Model for project screens. Load local notification package.

--- lib/models/project.dart
+++ lib/models/project.dart
@@ -3,7 +3,14 @@ class Project {
 }
 
 enum Type {
+  casual(code: 'casual', title: 'Casual'),
+  sport(code: 'sport', title: 'Sport'),
+  classic(code: 'classic', title: 'Classic');
 
+
+  final String code;
+  final String title;
+  const Type({required this.code, required this.title});
 }
 
 enum Category {
